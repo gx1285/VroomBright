199e8ce916fc3a79f6ffea1001b5e4eb23c5b3a1
Aroko <122141457+Aroko001@users.noreply.github.com>
2023-04-09 13:39:48 +0900

    Fix

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ async def bot_ready():
       [
         f"/help | {len(bot.guilds)} サーバー",
         f"/help | {len(bot.users)} ユーザー数",
-        f"/help | {round(bot.latency * 1000)}ms"
+        f"/help | {round(bot.latency * 1000)}ms",
       ]
     )
   )
