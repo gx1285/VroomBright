2ed2c36f889d81a09b61d72da157278fe63c62f1
Aroko <122141457+Aroko001@users.noreply.github.com>
2023-04-10 21:08:36 +0900

    Update uptime.py

--- cogs/uptime.py
+++ cogs/uptime.py
@@ -7,7 +7,7 @@ class uptime(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot: commands.Bot = bot
 
-    @app_commands.command(name="uptime", description="Botの起動時間を表示します。")
+    @app_commands.command(name="uptime", description="Botの起動時刻を表示します。")
     async def uptime(self, i: discord.Interaction):
         await i.response.send_message(f"<t:{self.bot.uptime}:F>")
 
