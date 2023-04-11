67a334aed2f15a230583875bcb72a8c9989f734b
Aroko <122141457+Aroko001@users.noreply.github.com>
2023-04-11 20:24:43 +0900

    update: 計算をしやすくしました。

--- cogs/calc.py
+++ cogs/calc.py
@@ -10,10 +10,18 @@ class calc(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot: commands.Bot = bot
 
+    @app_commands.choices(
+        mode=[
+            app_commands.Choice(name="足し算", value="+"),
+            app_commands.Choice(name="引き算", value="-"),
+            app_commands.Choice(name="掛け算", value="x"),
+            app_commands.Choice(name="割り算", value="/")
+        ]
+    )
     @app_commands.command(name="calc", description="計算します。")
-    @app_commands.describe(equation="計算したい数字を入力してください。(例: 10x10）")
-    async def calc(self, i: discord.Interaction, equation: str):
-        response = requests.get(f"http://api.wolframalpha.com/v2/query?appid={app_id}&input={equation}&output=json")
+    @app_commands.describe(num1="値を入力してください。", num2="値を入力してください。", mode="計算したいモードを選んでください。")
+    async def calc(self, i: discord.Interaction, num1: str, num2: str, mode: str):
+        response = requests.get(f"http://api.wolframalpha.com/v2/query?appid={app_id}&input={num1}{mode}{num2}&output=json")
         result = response.json()['queryresult']['pods'][1]['subpods'][0]['plaintext']
         conf_embed = discord.Embed(title="計算結果", color=discord.Color.green())
         conf_embed.add_field(name="結果", value=result)
